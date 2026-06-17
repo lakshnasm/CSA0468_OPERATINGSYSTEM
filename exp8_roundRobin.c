d8ce7d83caeaba4a6159392e3c43280b045a3064
lakshnasm <lakshnasm098@gmail.com>
2026-06-17 15:17:33 +0530

    Update exp8_roundRobin.c

--- exp8_roundRobin.c
+++ exp8_roundRobin.c
@@ -1,47 +1,78 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<conio.h>
 
 int main()
 {
-    int n, bt[20], rem[20];
-    int tq, i, complete=0;
+    int i, NOP, sum=0, count=0, y, quant;
+    int wt=0, tat=0;
+    int at[10], bt[10], temp[10];
+    float avg_wt, avg_tat;
 
-    printf("Enter number of processes: ");
-    scanf("%d",&n);
+    printf("Total number of process in the system: ");
+    scanf("%d", &NOP);
 
-    for(i=0;i<n;i++)
+    y = NOP;
+
+    for(i=0; i<NOP; i++)
     {
-        printf("Burst Time P%d: ",i+1);
-        scanf("%d",&bt[i]);
-        rem[i]=bt[i];
+        printf("\nEnter the Arrival and Burst time of the Process[%d]\n", i+1);
+
+        printf("Arrival time is: ");
+        scanf("%d", &at[i]);
+
+        printf("Burst time is: ");
+        scanf("%d", &bt[i]);
+
+        temp[i] = bt[i];
     }
 
-    printf("Enter Time Quantum: ");
-    scanf("%d",&tq);
+    printf("Enter the Time Quantum for the process: ");
+    scanf("%d", &quant);
 
-    while(complete<n)
+    printf("\nProcess No\tBurst Time\tTAT\tWaiting Time");
+
+    for(sum=0, i=0; y!=0;)
     {
-        complete=0;
-
-        for(i=0;i<n;i++)
+        if(temp[i] <= quant && temp[i] > 0)
         {
-            if(rem[i]>0)
-            {
-                if(rem[i]>tq)
-                {
-                    printf("P%d executed for %d units\n",i+1,tq);
-                    rem[i]-=tq;
-                }
-                else
-                {
-                    printf("P%d completed\n",i+1);
-                    rem[i]=0;
-                }
-            }
-
-            if(rem[i]==0)
-                complete++;
+            sum += temp[i];
+            temp[i] = 0;
+            count = 1;
         }
+        else if(temp[i] > 0)
+        {
+            temp[i] -= quant;
+            sum += quant;
+        }
+
+        if(temp[i] == 0 && count == 1)
+        {
+            y--;
+
+            printf("\nProcess No[%d]\t%d\t\t%d\t%d",
+                   i+1,
+                   bt[i],
+                   sum-at[i],
+                   sum-at[i]-bt[i]);
+
+            wt += sum-at[i]-bt[i];
+            tat += sum-at[i];
+            count = 0;
+        }
+
+        if(i == NOP-1)
+            i = 0;
+        else if(at[i+1] <= sum)
+            i++;
+        else
+            i = 0;
     }
+
+    avg_wt = (float)wt / NOP;
+    avg_tat = (float)tat / NOP;
+
+    printf("\nAverage Turn Around Time: %f", avg_tat);
+    printf("\nAverage Waiting Time: %f", avg_wt);
 
     return 0;
 }
